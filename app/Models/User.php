dd383f9237e184f7e2a3054ca3c9a77d228aef66
Gavin Kimpson <gkimpson@gmail.com>
2026-07-25 09:41:10 +0100

    feat(auth): add laravel sanctum for api token authentication
    Installs laravel/sanctum and publishes its config and migration,
    plus adds the api routes file for token-authenticated endpoints.

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Str;
 use Laravel\Fortify\Contracts\PasskeyUser;
 use Laravel\Fortify\PasskeyAuthenticatable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 /**
  * @property int $id
@@ -33,7 +34,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 class User extends Authenticatable implements PasskeyUser
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, PasskeyAuthenticatable, TwoFactorAuthenticatable;
+    use HasApiTokens, HasFactory, Notifiable, PasskeyAuthenticatable, TwoFactorAuthenticatable;
 
     /**
      * Get the attributes that should be cast.
